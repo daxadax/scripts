26d344a3901742b0ec8ed0a3822db7d8d41ffe4f
dax <d.dax@email.com>
2024-05-20 17:21:41 +0200

    Update keyboard switcher

--- switch_keyboard_layout.rb
+++ switch_keyboard_layout.rb
@@ -6,3 +6,10 @@ system("ibus engine #{toggle_engine}")
 
 # send real-time signal #7 to i3blocks to update keyboard layout block
 system('pkill -SIGRTMIN+7 i3blocks')
+
+# send a notification
+friendly = toggle_engine == 'hangul' ? 'Korean' : 'English'
+two_letter = toggle_engine == 'hangul' ? 'ko' : 'en'
+# TODO: would be nice to send a flag with the notification
+# system("notify-send -a keyboard-layout -t 1000 'Using #{friendly} keyboard layout' -i '~/programming/scripts/resources/#{two_letter}.png'")
+system("notify-send -a keyboard-layout -t 1000 'Using #{friendly} keyboard layout'")
